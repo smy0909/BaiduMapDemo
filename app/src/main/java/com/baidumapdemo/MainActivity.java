55564ea40e5c87a9f51534227ded0f17a8e75341
smy0909 <529104659@qq.com>
2019-11-14 15:45:45 +0800

    百度地图

---
+++ app/src/main/java/com/baidumapdemo/MainActivity.java
@@ -0,0 +1,157 @@
+package com.baidumapdemo;
+
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+
+import com.baidu.location.BDLocation;
+import com.baidu.location.BDLocationListener;
+import com.baidu.location.LocationClient;
+import com.baidu.location.LocationClientOption;
+import com.baidu.mapapi.map.BaiduMap;
+import com.baidu.mapapi.map.BitmapDescriptor;
+import com.baidu.mapapi.map.BitmapDescriptorFactory;
+import com.baidu.mapapi.map.MapStatus;
+import com.baidu.mapapi.map.MapStatusUpdateFactory;
+import com.baidu.mapapi.map.MapView;
+import com.baidu.mapapi.map.MyLocationConfiguration;
+import com.baidu.mapapi.map.MyLocationData;
+import com.baidu.mapapi.model.LatLng;
+
+public class MainActivity extends AppCompatActivity implements SensorEventListener {
+
+    private double mCurrentLat;
+    private double mCurrentLon;
+    private MapView mMapView;
+    private BaiduMap mBaiduMap;
+    private LocationClient mLocClient;
+    private float mCurrentAccracy;
+    private boolean isFirstLoc =true;
+    private float mCurrentDirection = 90f;
+    private SensorManager mSensorManager;
+    private MyLocationConfiguration.LocationMode mCurrentMode;
+    private MyLocationListener myListener = new MyLocationListener();
+    private MyLocationData myLocationData;
+    private double lastX;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        // 地图初始化
+        mMapView = (MapView) findViewById(R.id.bmapView);
+        mBaiduMap = mMapView.getMap();
+        // 获取传感器管理服务
+        mSensorManager = (SensorManager) getSystemService(SENSOR_SERVICE);
+        mCurrentMode = MyLocationConfiguration.LocationMode.NORMAL;
+        // 为系统的方向传感器注册监听器
+        mSensorManager.registerListener(this, mSensorManager.getDefaultSensor(Sensor.TYPE_ORIENTATION), SensorManager.SENSOR_DELAY_UI);
+        initLocation();
+    }
+
+    /**
+     * 定位初始化
+     */
+    public void initLocation() {
+        // 开启定位图层
+        mBaiduMap.setMyLocationEnabled(true);
+        // 定位初始化
+        mLocClient = new LocationClient(this);
+        mLocClient.registerLocationListener(myListener);
+        LocationClientOption option = new LocationClientOption();
+        // 打开gps
+        option.setOpenGps(true);
+        // 设置坐标类型
+        option.setCoorType("bd09ll");
+        option.setScanSpan(1000);
+        mLocClient.setLocOption(option);
+        mLocClient.start();
+    }
+
+    @Override
+    public void onSensorChanged(SensorEvent sensorEvent) {
+        double x = sensorEvent.values[SensorManager.DATA_X];
+        if (Math.abs(x - lastX) > 1.0) {
+            mCurrentDirection = (int) x;
+            myLocationData = new MyLocationData.Builder()
+                    .accuracy(mCurrentAccracy)// 设置定位数据的精度信息，单位：米
+                    .direction(mCurrentDirection)// 此处设置开发者获取到的方向信息，顺时针0-360
+                    .latitude(mCurrentLat)
+                    .longitude(mCurrentLon)
+                    .build();
+            mBaiduMap.setMyLocationData(myLocationData);
+        }
+        lastX = x;
+
+    }
+
+    @Override
+    public void onAccuracyChanged(Sensor sensor, int i) {
+
+    }
+
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        // 在activity执行onResume时必须调用mMapView. onResume ()
+        mMapView.onResume();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        // 在activity执行onPause时必须调用mMapView. onPause ()
+        mMapView.onPause();
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        // 取消注册传感器监听
+        mSensorManager.unregisterListener(this);
+        // 退出时销毁定位
+        mLocClient.stop();
+        // 关闭定位图层
+        mBaiduMap.setMyLocationEnabled(false);
+        // 在activity执行onDestroy时必须调用mMapView.onDestroy()
+        mMapView.onDestroy();
+    }
+
+    /**
+     * 定位SDK监听函数
+     */
+    public class MyLocationListener implements BDLocationListener {
+        @Override
+        public void onReceiveLocation(BDLocation location) {
+            // MapView 销毁后不在处理新接收的位置
+            if (location == null || mMapView == null) {
+                return;
+            }
+            mCurrentLat = location.getLatitude();
+            mCurrentLon = location.getLongitude();
+            mCurrentAccracy = location.getRadius();
+
+
+
+            myLocationData = new MyLocationData.Builder()
+                    .accuracy(location.getRadius())// 设置定位数据的精度信息，单位：米
+                    .direction(mCurrentDirection)// 此处设置开发者获取到的方向信息，顺时针0-360
+                    .latitude(location.getLatitude())
+                    .longitude(location.getLongitude())
+                    .build();
+            mBaiduMap.setMyLocationData(myLocationData);
+            if (isFirstLoc) {
+                isFirstLoc = false;
+                LatLng ll = new LatLng(location.getLatitude(), location.getLongitude());
+                MapStatus.Builder builder = new MapStatus.Builder();
+                builder.target(ll).zoom(18.0f);
+                mBaiduMap.animateMapStatus(MapStatusUpdateFactory.newMapStatus(builder.build()));
+            }
+        }
+    }
+}
